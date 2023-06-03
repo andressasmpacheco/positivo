6af91c5da8de83be7b8db73c28e3cf50db2c720d
Andressa Simões <73480001+andressasmpacheco@users.noreply.github.com>
2023-06-03 11:47:25 -0300

    Update positivo.c

--- positivo.c
+++ positivo.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-int main()
-{
-    int num, resto;
-     printf("Digite um numero e diremos se ele e positivo ou negativo\n\n");
-    scanf("%d", &num);
-    resto = num % 2;
-    if (resto==0)
-    {
-       printf("\nO numero %d e positivo. \n",num);
+
+int main() {
+    int numero;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &numero);
+
+    if (numero > 0) {
+        printf("O número é positivo.\n");
+    } else if (numero < 0) {
+        printf("O número é negativo.\n");
+    } else {
+        printf("O número é zero.\n");
     }
-    else
-    {
-        printf("\nO numero %d e negativo. \n",num);
-    }
-    
+
+    return 0;
 }
